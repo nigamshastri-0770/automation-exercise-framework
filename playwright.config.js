9b74e13298ce7a01e852a8806a5d1ccdfaa4449f
Nigam Shastri <macbookpro@Nigam-Shastri.local>
2026-05-22 11:30:43 +0530

    Added HTTPS ignore and updated Jenkins pipeline

--- playwright.config.js
+++ playwright.config.js
@@ -2,42 +2,20 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
 
-  testDir: './tests',
-
-  timeout: 30000,
-
   retries: 1,
 
-  fullyParallel: false,
-
-  reporter: [
-    ['list'],
-    ['html']
-  ],
-
   use: {
-
     browserName: 'chromium',
 
-    // Uses local Chrome browser
-    channel: 'chrome',
+    headless: true,
 
-    headless: false,
-
-    viewport: {
-      width: 1280,
-      height: 720
-    },
+    ignoreHTTPSErrors: true,
 
     screenshot: 'only-on-failure',
 
     trace: 'on-first-retry',
 
-    // DISABLE VIDEO
-    video: 'off',
-
-    launchOptions: {
-      slowMo: 300
-    }
+    video: 'retain-on-failure'
   }
+
 });
